c6c55947ae374600c0912e9fe7dafa8332256a07
deletreo <quaglinileo9@gmail.com>
2026-01-12 19:08:15 +0100

    resend api key is now set

--- src/app/api/send/route.tsx
+++ src/app/api/send/route.tsx
@@ -2,10 +2,7 @@ import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
 // TUA API KEY
-
-const RESEND_API_KEY = 're_Boeq4tmT_JvaYDXvTGnfRX6Z1bmfshevo'
-
-const resend = new Resend(RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
     try {
